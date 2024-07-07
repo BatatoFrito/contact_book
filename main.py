cf8a005d54bb360bdba4308ff6ca20e1eef5041a
Batato <batatofritogames@gmail.com>
2024-07-06 23:14:30 -0300

    remove() added

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ import os
 def clear():
     return os.system('cls')
 
+def pause():
+    return os.system('pause')
+
 class ContactBook:
     def __init__(self, contacts: dict, json_file: Path) -> None:
         self.contacts = contacts
@@ -28,8 +31,8 @@ class ContactBook:
         else:
             clear()
 
-        confirmation = input('Are you sure you want to create this contact? (Y/N): ')[0].upper()
-        if confirmation == 'Y':
+        confirmation = input('Are you sure you want to create this contact? (Y/N): ').upper()
+        if confirmation.startswith('Y'):
             self.history.append(deepcopy(self.contacts))
             self.contacts['contacts'].append(temp_contact)
             json_contacts = json.dumps(self.contacts)
@@ -43,18 +46,41 @@ class ContactBook:
 
     def remove(self):
         clear()
+        if not self.contacts['contacts']:
+            print('There are no contacts to delete...\n')
+            pause()
+            clear()
+            return
+
         index = 1
         for contact in self.contacts['contacts']:
             print(f"{index} - {contact['First Name']} {contact['Last Name']}")
             index += 1
         to_delete = input('\nWhich contact would you like to delete? (ID Number): ')
         clear()
-        confirmation = input('Are you sure you want to delete this contact?(Y/N): ')
-        try:
-            to_delete = int(to_delete)
-        except(TypeError, IndexError):
-            ...
 
+        confirmation = input('Are you sure you want to delete this contact?(Y/N): ').upper()
+        if confirmation.startswith('Y'):
+            try:
+                self.history.append(deepcopy(self.contacts))
+                to_delete = int(to_delete)
+                if to_delete <= 0:
+                    raise ValueError('Index has to be bigger than 0')
+                to_delete -= 1
+                self.contacts['contacts'].pop(to_delete)
+                json_contacts = json.dumps(self.contacts)
+                with open(self.json_file, 'w+') as f:
+                    f.truncate(0)
+                    f.seek(0)
+                    f.write(json_contacts)
+                    f.seek(0)
+            except(ValueError, IndexError):
+                self.history.pop()
+                clear()
+                print('Invalid contact ID...\n')
+                pause()
+        clear()
+    
     def edit():
         ...
 
@@ -76,9 +102,9 @@ except(FileNotFoundError):
         f.write('{'
                 '"contacts": ['
                     '{'
-                        '"First Name": "Marcio",'
-                        '"Last Name": "Jorge",'
-                        '"E-mail": "marciojorge@email",'
+                        '"First Name": "Example",'
+                        '"Last Name": "Example",'
+                        '"E-mail": "exemple@email",'
                         '"Phone": "123456789"'
                     '}'
                             ']'
