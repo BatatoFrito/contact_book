4630e477445168648fe93940f2c1ec9aeaf40f44
Batato <batatofritogames@gmail.com>
2024-07-07 00:30:10 -0300

    comments added

--- main.py
+++ main.py
@@ -33,17 +33,20 @@ class ContactBook:
                 ('U', 'UNDO', '[U]ndo last action'): self.undo,
                 ('C', 'CLOSE', '[C]lose the contact book'): close}
 
+    # Adds a contact
     def add(self):
+        # Creates the new contact temporarily
         temp_contact = {}
         for info in self.contact_info:
             clear()
             temp_info = input(f'{info}: ')
             temp_contact[info] = temp_info
-        else:
-            clear()
+        clear()
 
+        # Saves the new contact
         confirmation = input('Are you sure you want to create this contact? (Y/N): ').upper()
         if confirmation.startswith('Y'):
+            # Deepcopy added to the history
             self.history.append(deepcopy(self.contacts))
             self.contacts['contacts'].append(temp_contact)
             json_contacts = json.dumps(self.contacts)
@@ -54,8 +57,9 @@ class ContactBook:
                 f.seek(0)
         clear()
 
-
+    # Removes a contact
     def remove(self):
+        # Checks if there are contacts
         clear()
         if not self.contacts['contacts']:
             print('There are no contacts to delete...\n')
@@ -63,6 +67,7 @@ class ContactBook:
             clear()
             return
 
+        # Indexes the contacts and then asks the user which one it wants to select
         index = 1
         for contact in self.contacts['contacts']:
             print(f"{index} - {contact['First Name']} {contact['Last Name']}")
@@ -70,10 +75,13 @@ class ContactBook:
         to_delete = input('\nWhich contact would you like to delete? (ID Number): ')
         clear()
 
+        # Deletes the contact
         confirmation = input('Are you sure you want to delete this contact? (Y/N): ').upper()
         if confirmation.startswith('Y'):
             try:
+                # Deepcopy added to the history
                 self.history.append(deepcopy(self.contacts))
+                # Validates selected contact
                 to_delete = int(to_delete)
                 if to_delete <= 0:
                     raise ValueError('Index has to be bigger than 0')
@@ -92,7 +100,9 @@ class ContactBook:
                 pause()
         clear()
     
+    # Edits a contact's information
     def edit(self):
+        # Checks if there are contacts to edit
         clear()
         if not self.contacts['contacts']:
             print('There are no contacts to edit...\n')
@@ -100,12 +110,14 @@ class ContactBook:
             clear()
             return
 
+        # Indexes the contacts and then asks the user which one it wants to select
         index = 1
         for contact in self.contacts['contacts']:
             print(f"{index} - {contact['First Name']} {contact['Last Name']}")
             index += 1
         to_edit = input('\nWhich contact would you like to edit? (ID Number): ')
         
+        # Validates selected index
         try:
             to_edit = int(to_edit)
             if to_edit <= 0:
@@ -120,12 +132,14 @@ class ContactBook:
             clear()
             return
 
+        # Indexes the infos and then asks the user which one it wants to select
         index = 1
         for info in self.contact_info:
             print(f"{index} - {info}")
             index += 1
         to_edit_info = input('\nWhich info would you like to edit? (ID Number): ')
 
+        # Validates the selected info
         try:
             to_edit_info = int(to_edit_info)
             if to_edit_info <= 0:
@@ -140,11 +154,14 @@ class ContactBook:
             clear()
             return
         
+        # Asks the user what it wants the info to be edited into
         info_edit = input(f"What would you like the new {new_info} for {contact_edit['First Name']} {contact_edit['Last Name']} to be?: ")
         clear()
 
+        # Edits the contact
         confirmation = input('Are you sure you want to edit this contact? (Y/N): ').upper()
         if confirmation.startswith('Y'):
+            # Deepcopy added to the history
             self.history.append(deepcopy(self.contacts))
             contact_edit[new_info] = info_edit
             json_contacts = json.dumps(self.contacts)
@@ -155,7 +172,9 @@ class ContactBook:
                 f.seek(0)
         clear()
 
+    # Shows a contact's information
     def see(self):
+        # Indexes the contacts and then asks the user which one it wants to select
         clear()
         index = 1
         for contact in self.contacts['contacts']:
@@ -163,6 +182,7 @@ class ContactBook:
             index += 1
         to_see = input('\nWhich contact would you like to see? (ID Number): ')
 
+        # Validates the selected contact
         try:
             to_see = int(to_see)
             if to_see <= 0:
@@ -177,13 +197,16 @@ class ContactBook:
             clear()
             return
         
+        # Shows contact info
         for key, value in contact_see.items():
             print(f'{key} - {value}')
         print()
         pause()
         clear()
 
+    # Undoes last action
     def undo(self):
+        # Checks if there are actions to undo
         clear()
         if not self.history:
             print('There is nothing to undo...\n')
@@ -191,6 +214,7 @@ class ContactBook:
             clear()
             return
         
+        # Undoes it
         confirmation = input('Are you sure you want to undo last action? (Y/N): ').upper()
         if confirmation.startswith('Y'):
             clear()
@@ -206,6 +230,7 @@ class ContactBook:
             pause()
         clear()
 
+    # Brings up a menu to use the contact book
     def menu(self):
         clear()
         while True:
@@ -225,6 +250,7 @@ if __name__ == '__main__':
     FILE_PATH = Path(__file__).absolute().parent
     CONTACTS_PATH = FILE_PATH / 'contacts.json'
 
+    # Loads contact book data from contacts.json, creates new file if it doesn't exist
     try:
         with open(CONTACTS_PATH, 'r') as f:
             data = json.load(f)
